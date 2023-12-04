8bf8759f638446ed95f21c4e8e41b414bdcf39fb
Liiipe <133690633+Liiipe@users.noreply.github.com>
2023-12-03 22:24:13 -0300

    terceiro commit

--- eclipse-workspace/Project.Cad.Monitores1/src/main/Main.java
+++ eclipse-workspace/Project.Cad.Monitores1/src/main/Main.java
@@ -11,6 +11,7 @@ public class Main {
 	public static void main(String[] args) {
 		Persistencia persistencia = new Persistencia();
 		CentralDeTudo central = persistencia.recuperarCentral();
+		System.out.println("Estoi loko");
 		
 		//new	TelaPadrao("Kratos");
 		//TelaDeCadastro tela	= 	new TelaDeCadastro();
